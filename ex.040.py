37e1c5fc9f723105c8678a6cb07ff8a091251a15
Breno Barelli <breno.barelli4@gmail.com>
2025-06-17 12:51:42 -0300

    Exercício Python - Curso em Vídeo

--- ex.040.py
+++ ex.040.py
@@ -2,3 +2,14 @@
 #Média abaixo de 5.0: REPROVADO
 #Média entre 5.0 e 6.9: RECUPERAÇÃO
 #Média 7.0 ou superior: APROVADO
+
+n1 = float(input('Digite sua primeira nota: '))
+n2 = float(input('Digite sua segunda nota: '))
+media = (n1+n2) / 2
+
+if media > 7:
+    print(f'Parabéns você foi APROVADO! com média {media}')
+elif media > 5 and media <= 6.9:
+    print('Infelizmente você ficou de RECUPERAÇÃO, acreditamos em você')
+else:
+    print('Seu resultado não foi satisfatório você está REPROVADO') 
