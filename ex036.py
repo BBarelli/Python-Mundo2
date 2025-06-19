1eaeb4a30ce7e8158debc7522a5dd5b11f22a882
Breno Barelli <breno.barelli4@gmail.com>
2025-06-19 12:58:47 -0300

    Exercício Python - Curso em Vídeo

--- ex036.py
+++ ex036.py
@@ -8,7 +8,8 @@ prestacao = casa / (ano * 12)
 limite = salario * 0.3
 
 if prestacao > limite:  
-    print('Que pena a prestação excede o valor do seu salário.')
+    print(f'O imovél no valor de: {casa:.2f}, terá uma parcela de {prestacao:.2f} dentro do prazo {ano} anos')    
+    print('Parabéns temos margem de crédito pra você.')
 else: #No else não se passa condição:    
-    print(f'A casa custa {casa:.2f}, seu salário é de {salario:.2f}, dentro de {ano} anos')
-    print('Parabéns você foi contemplado!')
+    print(f'O imovél no valor de: {casa:.2f}, terá uma parcela de {prestacao:.2f} dentro do prazo {ano} anos')
+    print('Infelizmente a margem de crédito não foi liberada.')
