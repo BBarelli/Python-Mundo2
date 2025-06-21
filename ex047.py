dc11bb0865b08a3388242b21da165573867697a6
Breno Barelli <breno.barelli4@gmail.com>
2025-06-21 15:45:09 -0300

    Exercício Python - Curso em Vídeo

---
+++ ex047.py
@@ -0,0 +1,4 @@
+# Exercício Python 47: Crie um programa que mostre na tela todos os números pares entre 1 e 50.
+for c in range(1+1, 50+1, 2):
+    print(c)
+print('Esses são todos os números pares no intervalo de 1 a 50')
