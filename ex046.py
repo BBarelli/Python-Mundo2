836b8d95fa7056e77bf52868de7806fc400a98ee
Breno Barelli <breno.barelli4@gmail.com>
2025-06-21 15:26:50 -0300

    Exercício Python - Curso em Vídeo

--- ex046.py
+++ ex046.py
@@ -2,12 +2,10 @@
 # com uma pausa de 1 segundo
 import time
 
-print('ATENÇÃO A CONTAGEM REGRESSIVA  PARA O ANO DE 2026 IRÁ COMECAR EM BREVE!!!')
+print('⏰ ATENÇÃO A CONTAGEM REGRESSIVA  PARA O ANO DE 2026 IRÁ COMECAR EM BREVE!!! ⏰')
 print('Conte comigo......')
-print(' ')
 
 for c in range(10, 0, -1):
     print(c)
     time.sleep(1)
-print(' ')
-print('!!! FELIZ 2026 !!!!')
+print('💥💥💥!!! FELIZ 2026 !!!!💥💥💥')
