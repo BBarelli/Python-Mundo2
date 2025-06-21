85171458f136813b86e8ba2a7c14b9702747f677
Breno Barelli <breno.barelli4@gmail.com>
2025-06-21 16:04:12 -0300

    Exercício Python - Curso em Vídeo

--- ex048.py
+++ ex048.py
@@ -1,4 +1,10 @@
 # Exercício Python 48: Faça um programa que calcule a soma entre todos os números impares que são multiplos de três
 # e que se encontram no intervalo de 1 até 500
-
+s = 0
+for c in range(1, 501, 2):
+    if c % 3 == 0:
+        s = s + c # s += c
+print(f'O somatorio entre os valores impares é de: {s}')
+print('Fim')
+        
     
