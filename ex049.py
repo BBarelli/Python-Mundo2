cc6220dcdacec3cc126c8607028f67db87b2e47d
BBarelli <breno.barelli4@gmail.com>
2025-06-23 11:27:39 -0300

    Exercício Python - Curso em Vídeo

---
+++ ex049.py
@@ -0,0 +1,8 @@
+# Exercício Python 49: Refaça o desafio 009, mostrando a tabuada de um número que o usuário escolha, só que agora utilizando um laço for
+
+n = int(input('Digite o número que você deseja saber a tabuada: '))
+for c in range(1 , 11):
+    m = n * c
+    print(f' Tabuada do {n} * {c}: {m}')
+print('FIM')
+#  print(f' Tabuada do {n}: {m}')
