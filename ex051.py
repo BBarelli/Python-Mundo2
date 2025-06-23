fefdc965b494b01fee80df6293d9618882ca0b39
BBarelli <breno.barelli4@gmail.com>
2025-06-23 17:34:09 -0300

    Exercício Python - Curso em Vídeo

--- ex051.py
+++ ex051.py
@@ -1,3 +1,8 @@
 # Exercício Python 51: Desenvolva um programa que leia o primeiro termo e a razão de uma P.A
 # No final, mostre os 10 primeiro termos dessa progressão
-
+i = int(input('Digite o primeiro termo: '))
+r = int(input( 'Digite a razão: '))
+for c in range(0, 10):
+    termo = i + c * r
+    print(termo)
+print('FIM')
