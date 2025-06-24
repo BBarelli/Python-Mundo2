d7190ec76d6250664f161f594ca7aa4f21cd8569
BBarelli <breno.barelli4@gmail.com>
2025-06-24 15:24:05 -0300

    Exercício Python - Curso em Vídeo

--- ex053.py
+++ ex053.py
@@ -1,2 +1,14 @@
 # Exercício Python 53: Crie um programa que leia uma frase qualquer e diga se ela é um polidromo,
 #  desconsiderando os espaços 
+
+frase = str(input('Digite aqui sua frase: ')).strip().upper()
+palavra = frase.split() #utilizando o slip pra separa as palavras
+junto = ''.join(palavra) #utilizando o join pra juntar as palavas
+inverso = ''
+for letra in range (len(junto) -1, -1, -1):
+    inverso += junto[letra]
+print(f'O inverso de {junto} é {inverso}')
+if inverso == junto:
+    print('Temos um palidromo')
+else:
+    print('Não é um palidromo')
