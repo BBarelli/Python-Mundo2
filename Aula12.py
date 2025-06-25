18843fb172d1b4cf18607b663bb871aac3a3e7b7
BBarelli <breno.barelli4@gmail.com>
2025-06-25 11:34:25 -0300

    Estruturas Condiconais

--- Aula12.py
+++ Aula12.py
@@ -1,21 +1,21 @@
 
 # Criar estruturas condiconais aninhadas usando (if...elif).
 
-Condição simplificada:
+#Condição simplificada:
 if carro.esquerda(): #você pode ter um if sem else e vários elif
     bloco 1
 elif carro.direita(): # dentro do if pode usar quantos elif quiser. // nunca pode usar elif sem if
     bloco 2
-else: #se utilizar nenhuma ou apenas 1x! 
+else: #se utiliza nenhuma ou apenas 1x! 
     bloco 3
 
-Relembrando a condição simples:
+#Relembrando a condição simples:
 nome = str(input('Digite o seu nome: '))sr()
 if nome == Breno:
     print('Nossa que nome bonito você tem')
 print('Tenha um bom dia, {}!'.format(nome))
 
-Relembrando a condição composta:
+#Relembrando a condição composta:
 nome = str(input('Digite o seu nome: '))sr()
 if nome == Breno:
     print('Nossa que nome bonito você tem')
@@ -23,7 +23,7 @@ else:
     print('Seu nome é bem comum')
 print('Tenha um bom dia, {}!'.format(nome))
 
-Condição aninhada:
+#Condição aninhada:
 nome = str(input('Digite o seu nome: '))sr()
 if nome == Breno:
     print('Nossa que nome bonito você tem')
