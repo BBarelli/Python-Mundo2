9d2093a92a3d2845275309e689f9f320d944a6e4
Breno Barelli <breno.barelli4@gmail.com>
2025-07-15 23:34:48 -0300

    Mundo Python

--- ex058.py
+++ ex058.py
@@ -4,9 +4,20 @@
 
 from random import randint
 pc = randint(0, 10)
-print('Sou seu computador e acabei de pensar em um número entre 0 e 10')
+print('Olá vamos jogar o jogo da advinhação? pensei em algum número entre 0 e 10')
 print('Será que você consegue advinhar qual foi?')
-
 acertou = False
+palpite = 0
 
+#O laço enquanto não acertou
 while not acertou:
+    jogador = int(input('Digite seu palpite: '))
+    palpite += 1
+    if jogador == pc:
+        acertou = True
+    else:
+        if jogador < pc:
+            print('Mais..Tente novamente')
+        if jogador > pc:
+            print('Menos..Tente novamente')
+print(f'Certa resposta eu tinha escolhido o número {pc} e com apenas {palpite} palpites você acertou ')
---
+++ ex061.py
@@ -0,0 +1,6 @@
+#Exercício Python 61: Refaça o DESAFIO 51, lendo o primeiro termo e a razão de uma PA,
+#  mostrando os 10 primeiros termos da progressão usando a estrutura while.
+
+t = int(input('Digite um número como termo: '))
+r = int(input('Digite um número como razão: '))
+while c >
